7935bbc13718f22d6354440e9e37fdfe045a8f97
Just Mahmoud <142663291+justmahmoud31@users.noreply.github.com>
2024-04-27 14:03:25 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,126 +1,140 @@
 var siteName = document.getElementById("sitename");
 var siteurl = document.getElementById("siteurl");
 var sitedes = document.getElementById("sitedes");
-var submitbtn = document.getElementById('submitbtn');
-var errorbox = document.getElementById('errormessage');
+var submitbtn = document.getElementById("submitbtn");
+var errorbox = document.getElementById("errormessage");
 var deletebtn;
 var visitbtn;
 var bookmarks = [];
-function ok(){
-    errorbox.style.display="none";
+function ok() {
+  errorbox.style.display = "none";
 }
-function displayerror(){
-    errorbox.style.display="inline-block";
+function displayerror() {
+  errorbox.style.display = "inline-block";
 }
-function captial(str){
-    let strArr = str.split("");
-    strArr[0] = strArr[0].toUpperCase();
-    return strArr.join("");
+function captial(str) {
+  let strArr = str.split("");
+  strArr[0] = strArr[0].toUpperCase();
+  return strArr.join("");
 }
-if(localStorage.getItem("bookmarklist")) {
-    bookmarks = JSON.parse(localStorage.getItem("bookmarklist"));
+if (localStorage.getItem("bookmarklist")) {
+  bookmarks = JSON.parse(localStorage.getItem("bookmarklist"));
 }
- function takeinput(){
-    var print={
-        Name:captial(siteName.value),
-        url:siteurl.value,
-        description:sitedes.value
+function takeinput() {
+  var print = {
+    Name: captial(siteName.value),
+    url: siteurl.value,
+    description: sitedes.value,
+  };
+  if (print.description == "") {
+    print.description = "---";
+  }
+  if (print.Name == "" || print.url == "") {
+    displayerror();
+  } else {
+    bookmarks.push(print);
+  }
+  localStorage.setItem("bookmarklist", JSON.stringify(bookmarks));
+  swal("done", "Your bookmark has been added", "success");
+  display();
+  clear();
+}
+function clear() {
+  siteName.value = "";
+  siteurl.value = "";
+  sitedes.value = "";
+}
+function cancel() {
+  siteName.value = "";
+  siteurl.value = "";
+  sitedes.value = "";
+}
+function deleteitem(index) {
+  swal({
+    title: "Are you sure?",
+    text: "Once deleted, you will not be able to recover this Link",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  }).then((willDelete) => {
+    if (willDelete) {
+      bookmarks.splice(index, 1);
+      localStorage.setItem("bookmarklist", JSON.stringify(bookmarks));
+      display();
+      swal("Poof! Your imaginary Link has been deleted!", {
+        icon: "success",
+      });
+    } else {
+      swal("Your imaginary Link is safe!");
     }
-    if(print.description==""){
-      print.description="---";
-    }
-    if(print.Name==""||print.url==""){
-       displayerror()
-    }
-    else {
-        bookmarks.push(print);
-    }
-    localStorage.setItem('bookmarklist',JSON.stringify(bookmarks))
-    display()
-    clear()
-   
- }
- function clear(){
-    siteName.value="";
-    siteurl.value="";
-    sitedes.value="";
- }
- function cancel(){
-    siteName.value="";
-    siteurl.value="";
-    sitedes.value="";
+  });
 }
-function deleteitem(index){
-    bookmarks.splice(index,1)
-    localStorage.setItem('bookmarklist',JSON.stringify(bookmarks))
-    display()
-}
-function display(){
-    var all=``;
-    for(var i=0;i<bookmarks.length;i++){
-        all+=`
+function display() {
+  var all = ``;
+  for (var i = 0; i < bookmarks.length; i++) {
+    all += `
         <tr>
-            <td>${i+1}</td>
+            <td>${i + 1}</td>
             <td>${bookmarks[i].Name}</td>
             <td>${bookmarks[i].description}</td> 
             <td><button onclick="visititem(${i})" class="btn btn-info"><i class="fa-solid fa-arrow-up-right-from-square" style="color: #ededed;"></i></button></td>
             <td><button onclick="updateitem(${i})" class="btn btn-secondary"><i class="fa-solid fa-pen" style="color: #f5f5f5;"></i></button></td>
             <td><button onclick="deleteitem(${i})" class="btn btn-danger"><i class="fa-solid fa-trash" style="color: #e2e4e9;"></i></button></td>
         </tr>
-        `
-    }
-    document.getElementById("tableContent").innerHTML=all
+        `;
+  }
+  document.getElementById("tableContent").innerHTML = all;
 }
-display()
+display();
 visitbtn = document.getElementsByClassName(".btn.btn-info");
-if(visitbtn){
-    for(var i=0;i<visitbtn.length;i++){
-        visitbtn[i].addEventListener("click",function(e){
-            visititem(e);
-        })
-    }
+if (visitbtn) {
+  for (var i = 0; i < visitbtn.length; i++) {
+    visitbtn[i].addEventListener("click", function (e) {
+      visititem(e);
+    });
+  }
 }
-function visititem(index){
-  
-    open(bookmarks[index].url);
-      console.log(bookmarks[index].url);
-    // open(`https://${bookmarks[index].siteURL}`);
-
-
+function visititem(index) {
+  open(bookmarks[index].url);
+  // open(`https://${bookmarks[index].siteURL}`);
 }
-function updateitem(index){
+function updateitem(index) {
   // bookmarks[index].Name=siteName.value
   // bookmarks[index].url=siteurl.value
   // bookmarks[index].description=sitedes.value
   // if(bookmarks[index].description.value==""){
   //  bookmarks[index].description.value="---";
   // }
-  takeinput()
-  deleteitem(index)
-  localStorage.setItem('bookmarklist',JSON.stringify(bookmarks))
-  display()
-  clear()
+  takeinput();
+  deleteitem(index);
+  localStorage.setItem("bookmarklist", JSON.stringify(bookmarks));
+  display();
+  clear();
 }
-function searchitem(){
-  var srch=document.getElementById('srchinpt').value
-  var box=``
-  for(var i=0;i<bookmarks.length;i++){
-      if(bookmarks[i].Name.toLowerCase().includes(srch.toLowerCase())){
-        
-          box+=`
+function searchitem() {
+  var srch = document.getElementById("srchinpt").value;
+  var box = ``;
+  for (var i = 0; i < bookmarks.length; i++) {
+    if (bookmarks[i].Name.toLowerCase().includes(srch.toLowerCase())) {
+      box += `
           <tr>
-          <td>${i+1}</td>
-          <td>${bookmarks[i].Name.replace(srch,'<span style="background-color:#333; color:white;">'+srch+'</span>')}</td>
+          <td>${i + 1}</td>
+          <td>${bookmarks[i].Name.replace(
+            srch,
+            '<span style="background-color:#333; color:white;">' +
+              srch +
+              "</span>"
+          )}</td>
           <td>${bookmarks[i].description}</td>
           <td><button onclick="visititem(${i})" class="btn btn-info"><i class="fa-solid fa-arrow-up-right-from-square" style="color: #ededed;"></i></button></td>
-         <td><button onclick="updateitem(${i})" class="btn btn-secondary"><i class="fa-solid fa-pen" style="color: #f5f5f5;"></i></button></td>
+          <td><button onclick="updateitem(${i})" class="btn btn-secondary"><i class="fa-solid fa-pen" style="color: #f5f5f5;"></i></button></td>
           <td><button onclick="deleteitem(${i})" class="btn btn-danger"><i class="fa-solid fa-trash" style="color: #e2e4e9;"></i></button></td>
            </tr>
-          `
-      }
+          `;
+    }
   }
-  document.getElementById('tableContent').innerHTML=box
-  }
+  document.getElementById("tableContent").innerHTML = box;
+}
 var nRegex = /^\w{3,}(\s+\w+)*$/;
 // var Uegex = /^(https?:\/\/)?(w{3}\.)?\w+\.\w{2,}\/?(:\d{2,5})?(\/\w+)*$/;
 
@@ -162,13 +176,12 @@ document.addEventListener("click", function (e) {
     closeModal();
   }
 });
-let color= document.getElementById("bdy")
- function darkcolor() {
- color.style.backgroundColor="#333";
-alert("Dark")
-
- }
- function lightcolor() {
-    color.style.background="linear-gradient(114deg, rgba(114,193,199,1) 0%, rgba(132,69,252,1) 50%, rgba(222,103,228,1) 100%)";
-  
+let color = document.getElementById("bdy");
+function darkcolor() {
+  color.style.backgroundColor = "#333";
+  alert("Dark");
+}
+function lightcolor() {
+  color.style.background =
+    "linear-gradient(114deg, rgba(114,193,199,1) 0%, rgba(132,69,252,1) 50%, rgba(222,103,228,1) 100%)";
 }
